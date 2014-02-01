e91021faf32b0d5a7d565e1677d3a96d77f41e3e
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-02-01 14:31:27 -0200

    Initial Commit

---
+++ pong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pong
+//
+//  Created by Marcelo Sampaio on 2/1/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ pong/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  pong
+//
+//  Created by Marcelo Sampaio on 2/1/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
